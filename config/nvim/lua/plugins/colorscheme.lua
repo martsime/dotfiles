dd1dfa82180c3b9f9343175ebe623310e1ad13b3
Martin Simensen <martsime@users.noreply.github.com>
2023-08-07 19:30:13 +0200

    feat: Change colorscheme in neovim

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "olimorris/onedarkpro.nvim" },
+
+  -- Configure LazyVim to load onedarkpro
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
